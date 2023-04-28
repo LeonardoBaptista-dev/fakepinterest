b6aac228426d6b4fb91078aad200b1412af108f6
LeonardoBaptista-dev <leonardobaptista.dev@gmail.com>
2023-04-28 18:22:57 -0300

    3 commit

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.sqlite"
+app.config["SECRET_KEY"] = "6399b9e95f25fdc61795d7ff89ef3408"
+
+
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "index"
 
 
 from fakepinterest import routes
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from flask_login import login_required
 from fakepinterest import app 
 
 
@@ -7,7 +8,7 @@ def index():
     return render_template("index.html")
     
 
-
 @app.route("/perfil/<usuario>")
+@login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario, idade=25)
